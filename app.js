330f962582289d29e82728508d0595de68a2d6fc
Mary Harrell <harrell_mary@columbusstate.edu>
2019-12-03 21:11:31 +0000

    rats

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var http = require('http');
 var path = require('path');
-const express = require("express");
+//const express = require("express");
 var bodyParser = require("body-parser");
-const app = express();
+const nodeFetch = require( 'node fetch');
+
+const express = require('express');
+var app = express();
 const port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'htmls'));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ encoded: true}));
-var task = ["clean", "cook"];
-var complete = ["eat", "sleep"];
+app.use(bodyParser.urlencoded({ extended: true}));
+
 
 
 app.get('/', function(req, res){
-    res.render('index',{task:task, complete:complete});
+    let url = 'http://xkcd.com/info.0.json';
+    let img;
+    nodeFetch(url)
+        .then(resp =>resp.json())
+        .then(data => {
+            img = data.img;
+            res.render('index',{img:img});
+        })
+        .catch(err => {
+            console.log(err);
+            res.render('index');
+        })
+    
 });
 
 
